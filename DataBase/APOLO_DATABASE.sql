60d77540dfc94e143eb276eeab09414cca5defd0
Juan Beltran Garcia <jbeltrang1603@alumno.ipn.mx>
2021-12-05 20:49:23 -0600

    terminando base de datos

--- DataBase/APOLO_DATABASE.sql
+++ DataBase/APOLO_DATABASE.sql
@@ -20,42 +20,62 @@ create table AgenteSeguros(
 create table Contacto(
 
 	Id_Contacto int(3) not null auto_increment primary key,
-    Nombre_Contacto varchar(60)
-
-
-
+    Id_Agente int(3),
+    Nombre_Contacto varchar(60),
+    Correo_Electronico varchar(60),
+    Direccion varchar(100),
+    Telefono varchar(20),
+    Edad int(3),
+    Sexo varchar(20),
+    Estado_Civil varchar(30),
+    Familia varchar(2),
+    Polizas_Activas int(3),
+    Antecedentes_Financieros varchar(1000),
+    Antecedentes_Financieros varchar(1000),
+    Antecedentes_Financieros varchar(1000),
+    foreign key (Id_Agente) references AgenteSeguros (Id_Agente)
+    
 );
-
-
-
-
-
-
-
-
-
-
-
 
 
 create table Cita(
 
-	Id_
-
-
+	Id_Cita int(3) not null auto_increment primary key,
+    Id_Contacto int(3),
+    Id_Agente int(3),
+    Tipo_Cita varchar(50),
+    Fecha date,
+    Direccion_Cita varchar(100),
+    Proposito_Cita varchar(100),
+    Notas_Import varchar(200),
+    foreign key (Id_Contacto) references Contacto (Id_Contacto),
+    foreign key (Id_Agente) references AgenteSeguros (Id_Agente)
 
 );
 
 
+create table Ventas(
 
+	Id_Poliza int(3) not null auto_increment primary key,
+    Id_Contacto int(3),
+    Id_Agente int(3),
+	Aseguradora varchar(50),
+    Fecha_Venta date,
+    Tipo_Pago varchar(60),
+    Monto_Pago float(3,2),
+    foreign key (Id_Contacto) references Contacto (Id_Contacto),
+    foreign key (Id_Agente) references AgenteSeguros (Id_Agente)
 
-
-
+);
 
 
 
 
 select * from AgenteSeguros;
+
+
+
+
 
 use APOLO;
 drop procedure if exists GuardaAgente;
